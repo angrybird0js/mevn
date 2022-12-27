b809f8ebb037feebd7264012ca81994981b4a111
myplc <plctohmi@gmail.com>
2022-12-27 10:25:30 +0900

    정리

--- my01/src/views/HomeView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>Vue 프레임워크 whit 유성</h1>
-  </div>
-</template>
-
-<script></script>
-<style></style>
--- my05/src/views/AboutView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>Vue's lifecycle</h1>
-    <img src="../assets/vuejs_lifecycle.html.png" alt="" srcset="" />
-  </div>
-</template>
