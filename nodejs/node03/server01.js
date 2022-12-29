c535eff562ee5404463fc2781d629c338b2546e0
myplc <plctohmi@gmail.com>
2022-12-30 01:48:58 +0900

    1

--- nodejs/node03/server01.js
+++ nodejs/node03/server01.js
@@ -12,19 +12,18 @@ app.use(
   })
 )
 
-// module.exports = router
 const _path = path.join(__dirname, '/')
 console.log(_path)
 app.use('/', express.static(_path))
 app.use(logger('tiny'))
 
-let filename_ori
 const storage = multer.diskStorage({
   destination: (req, res, cb) => {
     cb(null, _path)
   },
   filename: (req, res, cb) => {
-    cb(null, res.originalname)
+    let on = res.originalname
+    cb(null, on)
   }
 })
 
@@ -32,6 +31,7 @@ let upload = multer({ storage: storage })
 
 app.post('/up', upload.single('ufile'), (req, res) => {
   console.log(req.file)
+  res.send(`<script>alert("파일 저장완료!");history.go(-1)</script>`)
 })
 
 app.listen(port, () => {
