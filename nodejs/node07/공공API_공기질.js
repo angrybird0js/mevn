64358a08222cab8534e8463d518569cce5bf4a97
myplc <plctohmi@gmail.com>
2023-01-09 09:12:41 +0900

    수업전

---
+++ nodejs/node07/공공API_공기질.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+let key = process.env.My_key
+
+const url =
+  'http://apis.data.go.kr/6260000/AirQualityInfoService/getAirQualityInfoClassifiedByStation?serviceKey=' +
+  key +
+  '&pageNo=1&numOfRows=10&resultType=json'
+console.log(url)
+request(url, (e, res, body) => {
+  const rst = JSON.parse(body)
+  const _ = rst.getAirQualityInfoClassifiedByStation.body.items.item[3]
+  console.log(
+    `[${_.site}]\n 일산화탄소:${_.co}\n 오존:${_.o3}\n 초미세먼지:${_.pm25}\n 미세먼지:${_.pm10}\n`
+  )
+})
