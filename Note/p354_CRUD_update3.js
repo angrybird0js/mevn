30d828a57bfb2e4ac563e82ce3541c3d65d7d6c0
myplc <plctohmi@gmail.com>
2023-01-16 14:46:55 +0900

    1

--- Note/p354_CRUD_update3.js
+++ Note/p354_CRUD_update3.js
@@ -2,16 +2,14 @@ const Photo = require('./p346_CRUD_ori.js')
 const main = async () => {
   const t = await Photo.updateMany(
     {
-      title: { $in: ['박서방', '홍길동', '배트맨', 'Johndoe'] }
-      // title: { $eq: '홍길동' }
-      // title: /홍길동/
     },
     {
-      $push:{ // 스키마에 필드가 미리 준비되어 있어야 한다.
-       "something": { $each: [1, 2, 3] } 
+      // $set:{ // 값을 입력
+      $inc:{ // 값을 증가.
+       "code": 3
     }
-  }
-    // { upsert: true, multi: true, new: true }
+  },
+    { upsert: true, multi: true, new: true }
   ).lean()
   console.log(t)
 }
